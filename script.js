a15f95b5395a767db011b357a5da78012af9fdbb
abderrahmanx <97347233+abderrahmanx@users.noreply.github.com>
2022-08-27 20:16:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const product= document.getElementById('text');
+
+function showdownarrow(){
+
+    document.getElementById('angel-down').style.opacity='0'
+    document.getElementById('angel-down').style.zIndex='50'
+    document.getElementById('angel-up').style.opacity='1'
+    document.getElementById('angel-up').style.opacity='51'
+  }
+  
+  function showdownarrow1()
+  { 
+    document.getElementById('angel-up').style.opacity='0'
+    document.getElementById('angel-up').style.zIndex='50'
+    document.getElementById('angel-down').style.opacity='1'
+    document.getElementById('angel-down').style.zIndex='51'
+  }
+   
+
+function arrowdown(){
+
+
+}
